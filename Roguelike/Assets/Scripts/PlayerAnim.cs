e0e59aadb058ff7d89a0a6f1e95e1e1897ba3f31
Newton Galiza <newtonjgaliza@gmail.com>
2023-01-26 15:14:25 -0300

    part 10
    part 10

---
+++ Roguelike/Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    private PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+        animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.moveInput != Vector2.zero)
+        {
+            animator.SetBool("isMoving", true);
+        }else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+      
+    }
+}
--- Roguelike/Assets/Scripts/PlayerController.cs
+++ Roguelike/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private Rigidbody2D rigidBody;
     [SerializeField] private Transform gunHand;
-    private Vector2 moveInput;
+    public Vector2 moveInput;
+    private Camera camera;
      
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = Camera.main;
     }
 
     // Update is called once per frame
@@ -23,23 +24,42 @@ public class PlayerController : MonoBehaviour
 
         MovementInput();
 
-        Vector3 mousePosition = Input.mousePosition;
-        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
-
-        //rotate the gun hand
-        Vector2 offset = new Vector2(mousePosition.x - screenPoint.x, mousePosition.y - screenPoint.y);
-        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-        gunHand.rotation = Quaternion.Euler(0f, 0f, angle);
+        MouseAim();
 
 
     }
 
     void MovementInput()
     {
-         moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.x= Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
 
         rigidBody.velocity = moveInput * moveSpeed;
+    }
+
+    void MouseAim()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 screenPoint = camera.WorldToScreenPoint(transform.localPosition);
+
+        //rotate the gun hand
+        Vector2 offset = new Vector2(mousePosition.x - screenPoint.x, mousePosition.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        gunHand.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        //rotate the player
+        if(mousePosition.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunHand.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gunHand.localScale = Vector3.one;
+            //Vector3.one é igual a Vector3(1f, 1f, 1f)
+
+        }
     }
 }
  
